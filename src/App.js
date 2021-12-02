8be7a804599cd494526154bb7fe8079d3c853e94
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-12-02 16:11:16 +0900

    EFFECTS | #6.0 ~ 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import styles from "./CSS/App.module.css";
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
+
+  function Hello() {
+    return <h1>Hello</h1>;
+  }
   return (
     <div>
-      <h1 className={styles.title}>W</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
