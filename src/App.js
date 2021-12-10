45c942c257b1d5b8c3ed4d763a2b91479f94d271
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-12-10 16:48:25 +0900

    To Do List(1)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { useEffect, useState } from "react";
-import Button from "./Button";
-import styles from "./CSS/App.module.css";
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = event => setToDo(event.target.value);
+  const onSubmit = event => {
+    event.preventDefault();
+    // 비었을 때는 변하지 않게끔
+    if (toDo === "") {
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]); // '...'은 배열을 그대로 가져온다
+    setToDo(""); // Submit하고 null로 바꿔주는 역할
+  };
+  console.log(toDos);
 
-  function Hello() {
-    return <h1>Hello</h1>;
-  }
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Do({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
